f8135581bafeaa13948ca94f9b17e51c2da5e79f
ZEN EX <zenex133@gmail.com>
2025-11-19 18:07:46 +0000

    نظام تسجيل الدخول

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -76,4 +76,4 @@ const CardFooter = React.forwardRef<
 ))
 CardFooter.displayName = "CardFooter"
 
-export { Cardthemed as Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter }
+export { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter }
