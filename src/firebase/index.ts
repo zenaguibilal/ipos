e6c6593036450fd4458eaf82a27e68c35fcc1a0f
ZEN EX <zenex133@gmail.com>
2025-11-19 13:11:52 +0000

    Try fixing this error: `Build Error: Export useUser doesn't exist in tar

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -54,6 +54,7 @@ export * from './provider';
 export * from './client-provider';
 export * from './firestore/use-collection';
 export * from './firestore/use-doc';
+export * from './auth/use-user';
 export * from './non-blocking-updates';
 export * from './errors';
 export * from './error-emitter';
