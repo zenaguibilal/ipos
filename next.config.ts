9211f1da225c705b00ea20cb7f35475068fda027
ZEN EX <zenex133@gmail.com>
2025-11-19 06:20:51 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type {NextConfig} from 'next';
 
 const config: NextConfig = {
   /* config options here */
-  experimental: {
-    allowedDevOrigins: [
-      'https://6000-firebase-studio-1763478698258.cluster-cbeiita7rbe7iuwhvjs5zww2i4.cloudworkstations.dev',
-    ],
-  },
   typescript: {
     ignoreBuildErrors: true,
   },
