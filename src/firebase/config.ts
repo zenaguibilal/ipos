aa77bccb03f73a433681b890844e1494c5309b8d
ZEN EX <zenex133@gmail.com>
2025-11-19 18:01:37 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1883806502-91d4a",
+  "appId": "1:59154120960:web:084cb72f6b81051ec533e5",
+  "apiKey": "AIzaSyC7LHWKYf4jrFXdrLLNbw8LIw_HzN6sB9w",
+  "authDomain": "studio-1883806502-91d4a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "59154120960"
+};
