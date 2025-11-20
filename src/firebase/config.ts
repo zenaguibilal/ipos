64c685f1f05dbac8507cd59069600a1c04a4ffef
ZEN EX <zenex133@gmail.com>
2025-11-20 18:55:25 +0000

    املأ ملف env.

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-1883806502-91d4a",
-  "appId": "1:59154120960:web:084cb72f6b81051ec533e5",
-  "apiKey": "AIzaSyC7LHWKYf4jrFXdrLLNbw8LIw_HzN6sB9w",
-  "authDomain": "studio-1883806502-91d4a.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "59154120960"
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
 };
