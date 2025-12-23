8df738ae90890ad925a54c12c2a625cbff784e78
ZEN EX <zenex133@gmail.com>
2025-12-23 14:13:57 +0000

    اين زر بتثبيت التطبيق كتطبيق اندرويد و تثبيت كبرنامج حاسوب

--- next.config.mjs
+++ next.config.mjs
@@ -1,41 +1,28 @@
-// @ts-check
+/** @type {import('next').NextConfig} */
 
-import withPWAInit from '@ducanh2912/next-pwa';
+import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  dest: 'public',
-  disable: process.env.NODE_ENV === 'development',
-  // add your own strategies to the existing ones
-  // cacheOnFrontEndNav: true,
-  // aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
-  swcMinify: true,
-  workboxOptions: {
-    disableDevLogs: true,
-  },
+  dest: "public",
+  disable: process.env.NODE_ENV === "development",
+  // add your own strategies
 });
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'lh3.googleusercontent.com',
-        port: '',
-        pathname: '/a/**',
-      },
-       {
-        protocol: 'https',
-        hostname: 'firebasestorage.googleapis.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-      }
-    ],
-  },
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'lh3.googleusercontent.com',
+                port: '',
+                pathname: '/a/**',
+            },
+             {
+                protocol: 'https',
+                hostname: 'picsum.photos',
+            }
+        ],
+    },
 };
 
 export default withPWA(nextConfig);
