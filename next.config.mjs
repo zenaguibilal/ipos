4c384860fe10d7c46304bea4161854aa537f35ee
ZEN EX <zenex133@gmail.com>
2026-01-14 17:36:46 +0000

    Try fixing this error: `Unhandled Runtime Error: Error: Invalid src prop

--- next.config.mjs
+++ next.config.mjs
@@ -1,58 +1,33 @@
 
+/** @type {import('next').NextConfig} */
 import withPWA from '@ducanh2912/next-pwa';
 
-/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // Your Next.js config...
+  reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/seed/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.vectorlogo.zone',
+        port: '',
+        pathname: '/logos/**',
+      }
+    ],
+  },
 };
 
-const withPWAConfig = withPWA({
+const pwaConfig = withPWA({
   dest: 'public',
   register: true,
   skipWaiting: true,
   disable: process.env.NODE_ENV === 'development',
-  runtimeCaching: [
-    {
-      urlPattern: new RegExp('^https://fonts.googleapis.com/.*', 'i'),
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'google-fonts-cache',
-        expiration: {
-          maxEntries: 10,
-          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-        },
-      },
-    },
-    {
-      urlPattern: new RegExp('^https://fonts.gstatic.com/.*', 'i'),
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'google-fonts-webfonts-cache',
-        expiration: {
-          maxEntries: 10,
-          maxAgeSeconds: 60 * 60 * 24 * 365, // 1 year
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:png|gif|jpg|jpeg|svg)$/,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'images',
-        expiration: {
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-        },
-      },
-    },
-    {
-      urlPattern: /\.(?:js|css)$/,
-      handler: 'StaleWhileRevalidate',
-      options: {
-        cacheName: 'static-resources',
-      },
-    },
-  ],
 });
 
-export default withPWAConfig(nextConfig);
+export default pwaConfig(nextConfig);
+
