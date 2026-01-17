86703dd4d714bee505878d44bb2ae1e3eb99f698
ZEN EX <zenex133@gmail.com>
2026-01-17 07:58:23 +0000

    حتى في الشاشات الكبيرة صفحة البيع لا تظهر كاملة و لا يمكن النزول الى الأ

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -11,7 +11,7 @@ export default function AppLayout({
   return (
       <FirebaseClientProvider>
         <div className="flex flex-col min-h-screen w-full">
-            <div className="flex flex-col h-screen max-h-screen overflow-hidden">
+            <div className="flex flex-col h-screen max-h-screen">
               <AppHeader />
               <main className="flex-1 overflow-auto bg-muted/40 pb-20 md:pb-0">{children}</main>
               <BottomNavBar />
