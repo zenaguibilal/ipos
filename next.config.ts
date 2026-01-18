c165fec1a20e8f75e6583b5c8f05bc58cd5c0fbc
ZEN EX <zenex133@gmail.com>
2026-01-18 18:46:30 +0000

    اكمل دعم تثبيت التطبيق على الحاسوب

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  disable: process.env.NODE_ENV === "development",
+  disable: false,
   register: true,
   cacheOnFrontEndNav: true,
   fallbacks: {
