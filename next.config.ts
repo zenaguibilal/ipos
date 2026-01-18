d77c4d6821367b062f0c4f784eba8381718df711
ZEN EX <zenex133@gmail.com>
2026-01-18 18:53:52 +0000

    19:52:07.264 Failed to compile. 19:52:07.265 Type error: Object literal

--- next.config.ts
+++ next.config.ts
@@ -10,36 +10,6 @@ const withPWA = withPWAInit({
   fallbacks: {
     document: "/offline",
   },
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/picsum\.photos\/.*/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "picsum-images",
-        expiration: {
-          maxEntries: 60,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-    {
-      urlPattern: /^https:\/\/www\.vectorlogo\.zone\/.*/i,
-      handler: "CacheFirst",
-      options: {
-        cacheName: "vector-logos",
-        expiration: {
-          maxEntries: 30,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-  ],
 });
 
 const nextConfig: NextConfig = {
