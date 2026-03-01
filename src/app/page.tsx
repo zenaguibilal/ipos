92c8440e438987d76503a9951ff876fd1c6cec41
ZEN EX <zenex133@gmail.com>
2026-03-01 09:04:26 +0000

    احذف الصفحة الرئيسية

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { LandingPage } from '@/components/landing/landing-page';
+import { redirect } from 'next/navigation';
 
 export default function HomePage() {
-    return <LandingPage />;
+  redirect('/sell');
 }
