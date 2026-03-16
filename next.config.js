20249a3599ab695279cb9109fce5da763d790e79
ZEN EX <zenex133@gmail.com>
2026-03-16 09:33:42 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,27 +1,3 @@
-const withPWA = require("@ducanh2912/next-pwa").default({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-  disable: process.env.NODE_ENV === "development",
-  runtimeCaching: [
-    {
-      urlPattern: /^https:\/\/picsum\.photos\/.*/i,
-      handler: 'CacheFirst',
-      options: {
-        cacheName: 'picsum-images-cache',
-        expiration: {
-          maxEntries: 100,
-          maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
-        },
-        cacheableResponse: {
-          statuses: [0, 200],
-        },
-      },
-    },
-  ],
-});
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     devIndicators: {
@@ -43,4 +19,4 @@ const nextConfig = {
     },
 };
 
-module.exports = withPWA(nextConfig);
+module.exports = nextConfig;
