c7ac351322dbbfb0971448c4bb3a871c234429de
ZEN EX <zenex133@gmail.com>
2026-03-23 21:23:03 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const withPWA = require("@ducanh2912/next-pwa")({
+const withPWA = require("@ducanh2912/next-pwa").default({
   dest: "public",
   register: false, // We use a custom PWA handler in PwaHandler.tsx
   skipWaiting: true,
