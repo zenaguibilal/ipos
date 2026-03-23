2f7ea3cbe44bec1a76a08ee3ce3a0110479ab48d
ZEN EX <zenex133@gmail.com>
2026-03-23 21:14:23 +0000

    اكمل pwa

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withPWA = require("@ducanh2912/next-pwa")({
+  dest: "public",
+  register: false, // We use a custom PWA handler in PwaHandler.tsx
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === "development",
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  workboxOptions: {
+    disableDevLogs: true,
+  },
+});
+
 const nextConfig = {
   reactStrictMode: true,
   typescript: {
@@ -29,4 +42,4 @@ const nextConfig = {
   }
 }
 
-module.exports = nextConfig
+module.exports = withPWA(nextConfig)
